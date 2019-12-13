966bfaf4a59a02ae5bd26b2d97208fa447459d08
gregf21 <gregory12006@yahoo.com>
2019-12-12 19:04:25 -0500

    Add project files.

---
+++ Slack VTPSInventory/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slack_VTPSInventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
